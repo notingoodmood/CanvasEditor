bb41475349ef1b799e5cd52477c71776023178dc
lvzhiwei02 <lvzhiwei02@meituan.com>
2021-03-11 14:32:17 +0800

    version 1.0

--- vuepro/vuepro/src/App.vue
+++ vuepro/vuepro/src/App.vue
@@ -1,73 +1,165 @@
 <template>
   <div id="app">
-    <h1 v-html = "title"></h1>
-    <input v-model="newItem" v-on:keyup.enter="addNew" ></input>
-    <ul>
-      <li v-for="item in items" v-bind:class="{finished:item.isFinished}" v-on:click="toggleFinish(item)">{{item.label}}</li>
-    </ul>
+    <div
+      v-bind:style="{
+        height: CanvasStyle.height + 'px',
+        width: CanvasStyle.width + 'px',
+        border: CanvasStyle.border,
+        position: CanvasStyle.position,
+      }"
+      class="myCanvas"
+    >
+      <vue-draggable-resizable
+        :w="value.width"
+        :h="value.height"
+        :x="value.x"
+        :y="value.y"
+        @dragging="onDrag"
+        @resizing="onResize"
+        @activated="onActivated(key)"
+        @deactivated="onDeactivated"
+        :parent="true"
+        v-for="(value, key) in DraggableComponentList"
+        v-bind:key="key"
+      >
+        <p>
+          Hello! I'm a flexible component. You can drag me around and you can
+          resize me.<br />
+          X: {{ value.x }} / Y: {{ value.y }} - Width: {{ value.width }} /
+          Height: {{ value.height }}
+        </p>
+      </vue-draggable-resizable>
+    </div>
+    <div>
+      <button v-on:click="onDraggableComponentCreate">添加一个元素</button>
+    </div>
+    <div>
+      <div v-for="(value, key) in DraggableComponentList" v-bind:key="key">
+        <div>元素 {{ key }}</div>
+        <div>
+          <span>水平坐标(x):</span>
+          <input class="input_x" :value="DraggableComponentList[key].x" />
+        </div>
+        <div>
+          <span>竖直坐标(y):</span>
+          <input class="input_y" :value="DraggableComponentList[key].y" />
+        </div>
+        <div>
+          <span>高度(height):</span>
+          <input
+            class="input_height"
+            :value="DraggableComponentList[key].height"
+          />
+        </div>
+        <div>
+          <span>宽度(width):</span>
+          <input
+            class="input_width"
+            :value="DraggableComponentList[key].width"
+          />
+        </div>
+      </div>
+      <button class="smaller_canvas" v-on:click="onResizeCanvas">
+        画布尺寸减半
+      </button>
+    </div>
   </div>
- 
 </template>
 
-
 <script>
-import Store from './store'
+import "vue-draggable-resizable/dist/VueDraggableResizable.css";
 export default {
-  data:function(){
+  data: function () {
     return {
-      title:"This is a Todolist",
-      items:Store.fetch(),
-      newItem:""
-    }
-  },
-  watch:{
-    items:{
-      handler:function(items){
-        Store.save(items)
+      CanvasStyle: {
+        height: 800,
+        width: 800,
+        border: "1px solid red",
+        position: "relative",
       },
-      deep:true
-    }
+      WarningColor: "crimson",
+      PlainColor: "black",
+      DraggableComponentList: [],
+      ActivitedInstance: 0,
+      Activited: false,
+      X: 0,
+      Y: 0,
+      Height: 0,
+      Width: 0,
+    };
   },
-  methods:{
-    toggleFinish:function(item){
-      item.isFinished = !item.isFinished
+  methods: {
+    onDraggableComponentCreate: function () {
+      let myComponent = new DraggableData(0, 0, 300, 300);
+      this.DraggableComponentList.push(myComponent);
     },
-    addNew:function(){
-      this.items.push({
-        label:this.newItem,
-        "isFinished":false 
-      })
-      this.newItem=""
-    }
+    onResize: function (x, y, width, height) {
+      let myInstance = this.DraggableComponentList[this.ActivitedInstance];
+      myInstance.x = x;
+      myInstance.y = y;
+      myInstance.width = width;
+      myInstance.height = height;
+    },
+    onActivated: function (val) {
+      this.ActivitedInstance = val;
+      this.Activited = true;
+    },
+    onDeactivated: function () {
+      //this.Activited = false;
+    },
+    onInputFocus: function () {
+      console.log("AAAAAAA");
+      this.Activited = true;
+    },
+    onDrag: function (x, y) {
+      let myInstance = this.DraggableComponentList[this.ActivitedInstance];
+      myInstance.x = x;
+      myInstance.y = y;
+    },
+    onResizeCanvas: function () {
+      this.CanvasStyle.height = this.CanvasStyle.height / 2;
+      this.CanvasStyle.width = this.CanvasStyle.width / 2;
+    },
+  },
+};
+class DraggableData {
+  x = 0;
+  y = 0;
+  width = 100;
+  height = 100;
+  display = true;
+  background_color = "#FFFFFF";
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
   }
 }
 </script>
 
 <style>
-.finished{
-  text-decoration:line-through;
+.finished {
+  text-decoration: line-through;
 }
-li{
-  list-style:none;
-  font-size:1.6em;
-  margin-top:10px;
+li {
+  list-style: none;
+  font-size: 1.6em;
+  margin-top: 10px;
+}
+.myCanvas {
+  float: left;
 }
 
 #app {
-  background-image:url(./576df1167c887_1024.jpg);
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /*
+  background-image: url(./576df1167c887_1024.jpg); 
+  */
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-input{
-  width:230px;
-  height:40px;
-  border-radius:20px;
-  padding: 0.4em 0.35em;
-  border:3px solid #CFCFCF;
-  font-size: 1.55em;
 }
 </style>
--- vuepro/vuepro/src/main.js
+++ vuepro/vuepro/src/main.js
@@ -1,13 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from "vue";
+import App from "./App";
+import Canvas from "./Canvas";
 
-Vue.config.productionTip = false
+//加入 VueDraggableResizable 插件
+import VueDraggableResizable from "vue-draggable-resizable";
+// optionally import default styles
+import "vue-draggable-resizable/dist/VueDraggableResizable.css";
+Vue.component("vue-draggable-resizable", VueDraggableResizable);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>",
+});
